c85e6670a57ca849080a33447faf2cf2d245afe9
mitra tabatabaei <mitra.tabatabaei.asl@gmail.com>
2020-03-16 13:14:40 +0330

    creating different function for attack

---
+++ assets/scripts/app.js
@@ -0,0 +1,29 @@
+const maxLife = 100;
+const PLAYER_ATTACK_VAlue = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+
+let currentPlayerHealth = maxLife;
+let currentMonsterHealth = maxLife;
+
+
+function endRound(){
+    initialPlayerLife = currentPlayerHealth;
+    const playerDamage = dealPlayerDamage (MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+}
+
+
+function attackMonster(){
+    initialMonsterLife = currentMonsterHealth;
+    const Monsterdamage = dealMonsterDamage(PLAYER_ATTACK_VAlue);
+    currentMonsterHealth -= Monsterdamage;
+}
+
+function strongAttackMonster(){
+    initialMonsterLife = currentMonsterHealth;
+    const monsterIntenseDamage = dealMonsterDamage (STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= monsterIntenseDamage;
+}
